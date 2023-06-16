42c5ca9f8515e19a070f9a5345ca081c1ead144c
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2023-06-16 20:41:43 +0600

    add image into home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <div className="banner">
+        <img
+          style={{ width: "100%", height: "auto" }}
+          src="https://img.freepik.com/free-psd/special-sales-banner-template_23-2148975924.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
